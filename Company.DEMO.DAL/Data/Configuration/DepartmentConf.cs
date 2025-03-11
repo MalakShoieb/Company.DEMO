06a49d4422673a2705cb046be02327daec33e029
MalakShoieb <malakshoieb456@gmail.com>
2025-03-11 08:18:57 +0200

    implementation of data access layer

---
+++ Company.DEMO.DAL/Data/Configuration/DepartmentConf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DEMO.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.DEMO.DAL.Data.Configuration
+{
+    internal class DepartmentConf : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(e => e.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
